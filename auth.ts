981764c2d2f796353e94c35d32b410d1ee8d1903
saurabh anand <saurabhbebi@gmail.com>
2024-04-14 13:48:42 +0530

    tokenID fix

--- auth.ts
+++ auth.ts
@@ -1,8 +1,8 @@
-import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./utils/db"
-
+import NextAuth from "next-auth"
+ 
 export const {
   handlers: { GET, POST },
   auth,signIn,signOut
@@ -19,10 +19,12 @@ export const {
   },
   callbacks:{
     async session({session,token}){
+      if(token.sub && session.user ){
+        session.user.id=token.sub
+      }
       return session;
     },
     async jwt({token}){
-    token.id=token.sub
       return token
     }
   },
