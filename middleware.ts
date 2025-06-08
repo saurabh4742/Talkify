8daf89efc2e43fbf2c997474b7c4dc5e882df7fb
saurabh anand <saurabhbebi@gmail.com>
2025-06-08 13:19:42 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -83,8 +83,8 @@ if (!isAllowedRequest) {
     if (nextUrl.search) {
       callbackUrl += nextUrl.search;
     }
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-    return Response.redirect(new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl));
+    // const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+    return Response.redirect(new URL(`/auth/login`, nextUrl));
   }
 
   return;
